03cf47aea08b89b31cbe105da55d222ed3cabec8
Joshua Kenneth Van Dyon <71171011+jkvd1@users.noreply.github.com>
2024-04-01 16:42:19 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+    home: HomePage(),
+  }
+}
+
+//Home Page
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: LoginScreen(),
+    );
+  }
+}
+
+//Login Screen
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            "MogokYa",
+            style: TextStyle(
+              color: Colors.black, 
+              fontSize: 28.0, 
+              fontWeight: FontWeight.bold,
+              ),
+            ),
+            
+          ],
+      ),
+    );
+  }
+}
+
+//
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
